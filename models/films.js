718abb6c5729a9656b5b6de0fb88fe3844a13d65
SergioIznaolaDeveloper <sergioiznaola@gmail.com>
2022-03-22 13:34:01 +0100

    pruebas_git_ignore

--- models/films.js
+++ models/films.js
@@ -4,36 +4,13 @@ const apiKey = process.env.API_KEY;
 
 const getDefaultFilm = async () => {
   try {
-    let response = await fetch(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`
-    ); //{}
-    let title = await response.json(); //{}
-    console.log(title);
-    return title;
+    console.log("hola")
   } catch (err) {
     console.log(err);
     throw err;
   }
 };
 
-// app.get("/films/:title?", async (req, res) => {
-//   let defaultFilm = req.params.title || "Sergio";
-//   try {
-//     let response = await fetch(
-//       `https://www.omdbapi.com/?t=${defaultFilm}&apikey=${apiKey}`
-//     ); //{}
-//     let title = await response.json(); //{}
-//     res.render("films", { films: title }); // Pinta datos en el pug
-//   } catch (error) {
-//     console.log(`ERROR: ${error.stack}`);
-//   }
-// });
-// /* recoger datos de input y redirección*/
-// app.post("/", function (req, res) {
-//   let film = req.body.films;
-//   console.log(film);
-//   res.redirect(`http://localhost:3000/films/${film}`);
-// });
 
 const entries = {
   getDefaultFilm,
